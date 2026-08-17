d2c1bbc6b13947d45e8842f74aac1593b830d52c
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2026-08-17 23:33:22 +0530

    Build a simple sequential workflow using langgrpah

---
+++ sequential_workflow/llm_workflow.py
@@ -0,0 +1,98 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# -----------------------------
+# 1. Create the Hugging Face LLM
+# -----------------------------
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation",
+    max_new_tokens=512,
+    temperature=0.2
+)
+
+model = ChatHuggingFace(llm=llm)
+
+
+# -----------------------------
+# 2. Define the State
+# -----------------------------
+
+class LLMState(TypedDict):
+    question: str
+    answer: str
+
+
+# -----------------------------
+# 3. Define the Node
+# -----------------------------
+
+def llm_qa(state: LLMState) -> LLMState:
+
+    question = state["question"]
+
+    prompt = f"Answer the following question: {question}"
+
+    answer = model.invoke(prompt).content
+
+    state["answer"] = answer
+
+    return state
+
+
+# -----------------------------
+# 4. Create the Graph
+# -----------------------------
+
+graph = StateGraph(LLMState)
+
+# Add node
+graph.add_node("llm_qa", llm_qa)
+
+# START → llm_qa
+graph.add_edge(START, "llm_qa")
+
+# llm_qa → END
+graph.add_edge("llm_qa", END)
+
+# Compile the graph
+app = graph.compile()
+
+
+# -----------------------------
+# 5. Main Function
+# -----------------------------
+
+def main():
+
+    print("Your Question Answer Chatbot")
+    print("Type 'exit' to quit.")
+
+    while True:
+
+        user_input = input("\nYou: ")
+
+        if user_input.lower() == "exit":
+            print("Goodbye!")
+            break
+
+        # Send input to LangGraph
+        result = app.invoke({
+            "question": user_input,
+            "answer": ""
+        })
+
+        print("AI:", result["answer"])
+
+
+# -----------------------------
+# 6. Run the program
+# -----------------------------
+
+if __name__ == "__main__":
+    main()
--- sequential_workflow/main.py
+++ sequential_workflow/main.py
@@ -5,6 +5,7 @@ class BMICalculator(TypedDict):
     weight_kg: float 
     height_m: float
     bmi: float
+    category: str
 
 
 def calculateBmi(state: BMICalculator) -> BMICalculator: 
@@ -15,15 +16,31 @@ def calculateBmi(state: BMICalculator) -> BMICalculator:
     state['bmi'] = round(bmi, 2)
     return state
 
+def label_bmi(state: BMICalculator) -> BMICalculator:
+    bmi = state['bmi']
+
+    if bmi < 18.5: 
+        state['category'] = "Underweight"
+    elif 18.5 <= bmi <= 24: 
+        state['category'] = "Normal"
+    elif 25 <= bmi <= 30: 
+        state['category'] = "Overweight"
+    else: 
+        state['category'] = "Obses"
+
+    return state
+
 # define your graph 
 graph = StateGraph(BMICalculator)
 
 # define your node 
 graph.add_node("calculate_bmi", calculateBmi) 
+graph.add_node("label_bmi", label_bmi)
 
 # define your edges 
 graph.add_edge(START, 'calculate_bmi')
-graph.add_edgej('calculate_bmi', END)
+graph.add_edge('calculate_bmi', 'label_bmi')
+graph.add_edge('label_bmi', END)
 
 # compile your graph
 workflow = graph.compile()
@@ -35,3 +52,4 @@ result = workflow.invoke({
 })
 
 print(result['bmi'])
+print(result['category'])
